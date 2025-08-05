7524f897a0e9f4262611c3a7d069d5c1c12e6d41
SatooRu65536 <ryuto65536@gmail.com>
2025-08-05 14:26:22 +0900

    処理を削除

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from pathlib import Path
-import numpy as np
-import pandas as pd
 from src.results import Results
 from src.data_provider import DataProvider
 from src.type import Position
-from scipy import signal
 
 
 def main():
@@ -24,51 +21,10 @@ def main():
         initial_position=Position(41.368, -10.047, 0.935),
     )
 
-    window_acc = 60
-    window_gyro = 60
-    peak_distance = 30
-    peak_height = 1.0
-    step = 0.4
-    init_angle = np.deg2rad(80)
-
     for acce_df, gyro_df, acce_all_df, gyro_all_df in dataprovider:
-        gyro_fs = gyro_all_df["app_timestamp"].count() / (
-            gyro_all_df["app_timestamp"].max() - gyro_all_df["app_timestamp"].min()
-        )
-
-        # 位置推定
-        acce_all_df["norm"] = acce_all_df[["x", "y", "z"]].apply(norm, axis=1)
-        gyro_all_df["norm"] = gyro_all_df[["x", "y", "z"]].apply(norm, axis=1)
-
-        gyro_all_df["angle"] = gyro_all_df["x"].cumsum() / gyro_fs
-
-        acce_all_df["low_norm"] = acce_all_df["norm"].rolling(window=window_acc).mean()
-        gyro_all_df["low_x"] = gyro_all_df["x"].rolling(window=window_gyro).mean()
-        gyro_all_df["low_angle"] = (
-            gyro_all_df["angle"].rolling(window=window_gyro, center=True).mean()
-        )
-
-        peaks, _ = signal.find_peaks(
-            acce_all_df["low_norm"], distance=peak_distance, height=peak_height
-        )
-        track = [results[0]]
-        for p in peaks:
-            time = acce_all_df["app_timestamp"][p]
-            gyro_i = gyro_all_df["app_timestamp"].sub(time).abs().idxmin()
-
-            x = step * np.cos(gyro_all_df["angle"][gyro_i] + init_angle) + track[-1][0]
-            y = step * np.sin(gyro_all_df["angle"][gyro_i] + init_angle) + track[-1][1]
-
-            track.append(Position(x, y, results[0].z))
-
-        # 推定結果を保存
-        results.track = track
+        pass
 
     results.plot_map()
-
-
-def norm(row: pd.Series):
-    return (row["x"] ** 2 + row["y"] ** 2 + row["z"] ** 2) ** 0.5
 
 
 if __name__ == "__main__":
