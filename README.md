161687d4c98d93a7e29a96624b4aa4cd59cd9cf3
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-08-05 16:47:54 +0900

    初期設定

--- README.md
+++ README.md
@@ -1,25 +1,31 @@
-# xDR Challenge 環境を再現したPDR環境
-xDR Challenge でPDRを実装するための環境を再現したものです。
+# xDR Challenge 環境を再現した PDR 環境
+
+xDR Challenge で PDR を実装するための環境を再現したものです。
 xDR Challenge では 0.5s ごとに送られてくるデータをリアルタイムで位置推定する必要があります。
 この環境では、加速度センサとジャイロセンサのデータを使用できる範囲(未来を除く過去~現在の範囲)だけを返すようにしています。
 
 ## セットアップ
+
 仮想環境の作成
+
 ```bash
 python -m venv venv
 source venv/bin/activate
 ```
 
 依存関係のインストール
+
 ```bash
 pip install -r requirements.txt
 ```
 
 ## 使い方
+
 ```py
 dataprovider = DataProvider(maxwait=0.5)
 results = Results()
 ```
+
 xDR Challenge では 0.5s ごとにデータを取得し、位置推定を行います。
 `maxwait` パラメータを指定することで、取得データの間隔を調整できます。
 
@@ -29,7 +35,8 @@ xDR Challenge では 0.5s ごとにデータを取得し、位置推定を行い
 ```py
 for acce_df, gyro_df, acce_all_df, gyro_all_df in dataprovider:
 ```
-- `acc_df`: 新たに取得した加速度
+
+- `acce_df`: 新たに取得した加速度
 - `gyro_df`: 新たに取得した角速度
 - `acce_all_df`: 過去から現在までの加速度
 - `gyro_all_df`: 過去から現在までの角速度
@@ -37,16 +44,19 @@ for acce_df, gyro_df, acce_all_df, gyro_all_df in dataprovider:
 ```py
 position = Position(x=x, y=y, z=z)
 ```
+
 位置を表す名前付きタプルを作成します。
 
 ```py
 results.append(position)
 ```
+
 位置推定の結果を保存します。
 
 ```py
 results.plot_map()
 # results.plot_map("output_map.png")
 ```
+
 位置推定の結果をマップ上にプロットします。
 ファイル名を指定することで、画像ファイルとして保存することもできます。
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from scipy import signal
+import numpy as np
 from src.results import Results
 from src.data_provider import DataProvider
 from src.type import Position
@@ -21,8 +23,38 @@ def main():
         initial_position=Position(41.368, -10.047, 0.935),
     )
 
+    window_acc = 60
+    window_gyro = 60
+    step = 0.3
+    peak_distance_sec = 0.5
+    peak_height = 1.0
+    init_angle = np.deg2rad(75)
+
     for acce_df, gyro_df, acce_all_df, gyro_all_df in dataprovider:
-        pass
+        acce_fs = acce_all_df['app_timestamp'].count() / (acce_all_df['app_timestamp'].max() - acce_all_df['app_timestamp'].min())
+        gyro_fs = gyro_all_df['app_timestamp'].count() / (gyro_all_df['app_timestamp'].max() - gyro_all_df['app_timestamp'].min())
+
+        acce_all_df['norm'] = (acce_all_df['x']**2 + acce_all_df['y']**2 + acce_all_df['z']**2)**(1/2)
+        gyro_all_df['norm'] = (gyro_all_df['x']**2 + gyro_all_df['y']**2 + gyro_all_df['z']**2)**(1/2)
+
+        gyro_all_df['angle'] = np.cumsum(gyro_all_df['x']) / gyro_fs
+
+        acce_all_df['low_norm'] = acce_all_df['norm'].rolling(window= window_acc).mean()
+        gyro_all_df['low_angle'] = gyro_all_df['angle'].rolling(window=window_gyro).mean()
+
+        peaks, _ = signal.find_peaks(acce_all_df['low_norm'], distance=acce_fs*peak_distance_sec, height=peak_height)
+
+        track=[results[0]]
+        for p in peaks:
+            # 加速度と角速度のインデックス数に合わせる
+            time = acce_all_df['app_timestamp'][p]
+            gyro_i = gyro_all_df['app_timestamp'].sub(time).abs().idxmin()
+
+            x = step * np.cos(gyro_all_df['angle'][gyro_i] + init_angle) + track[-1][0]
+            y = step * np.sin(gyro_all_df['angle'][gyro_i] + init_angle) + track[-1][1]
+
+            track.append(Position(x,y,0))
+        results.track = track
 
     results.plot_map()
 
