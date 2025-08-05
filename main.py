989e8861fed9cef05bb914537e010e7987efc1d7
SatooRu65536 <ryuto65536@gmail.com>
2025-08-05 17:36:11 +0900

    feat: 最低限のPDR

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-from scipy import signal
 import numpy as np
+from scipy import signal
 from src.results import Results
 from src.data_provider import DataProvider
 from src.type import Position
@@ -20,40 +20,59 @@ def main():
     )
     results = Results(
         map_file=map_file,
-        initial_position=Position(41.368, -10.047, 0.935),
+        initial_position=Position(41.368, -10.047, 0),
     )
 
-    window_acc = 60
-    window_gyro = 60
-    step = 0.3
-    peak_distance_sec = 0.5
-    peak_height = 1.0
-    init_angle = np.deg2rad(75)
+    window_acc_sec = 1.0  # 加速度の移動平均フィルタのウィンドウサイズ（秒）
+    window_gyro_sec = 1.0  # 角速度の移動平均フィルタのウィンドウサイズ（秒）
+    step = 0.4  # 歩幅（メートル）
+    peak_distance_sec = 0.5  # ピーク検出の最小距離（秒）
+    peak_height = 1.0  # ピーク検出の最小高さ
+    init_angle = np.deg2rad(80)  # 初期角度（ラジアン）
 
     for acce_df, gyro_df, acce_all_df, gyro_all_df in dataprovider:
-        acce_fs = acce_all_df['app_timestamp'].count() / (acce_all_df['app_timestamp'].max() - acce_all_df['app_timestamp'].min())
-        gyro_fs = gyro_all_df['app_timestamp'].count() / (gyro_all_df['app_timestamp'].max() - gyro_all_df['app_timestamp'].min())
+        # サンプリング周波数の計算
+        acce_fs = acce_all_df["app_timestamp"].count() / (
+            acce_all_df["app_timestamp"].max() - acce_all_df["app_timestamp"].min()
+        )
+        gyro_fs = gyro_all_df["app_timestamp"].count() / (
+            gyro_all_df["app_timestamp"].max() - gyro_all_df["app_timestamp"].min()
+        )
 
-        acce_all_df['norm'] = (acce_all_df['x']**2 + acce_all_df['y']**2 + acce_all_df['z']**2)**(1/2)
-        gyro_all_df['norm'] = (gyro_all_df['x']**2 + gyro_all_df['y']**2 + gyro_all_df['z']**2)**(1/2)
+        # ノルムの計算
+        acce_all_df["norm"] = (
+            acce_all_df["x"] ** 2 + acce_all_df["y"] ** 2 + acce_all_df["z"] ** 2
+        ) ** (1 / 2)
 
-        gyro_all_df['angle'] = np.cumsum(gyro_all_df['x']) / gyro_fs
+        # 角度の計算
+        gyro_all_df["angle"] = np.cumsum(gyro_all_df["x"]) / gyro_fs
 
-        acce_all_df['low_norm'] = acce_all_df['norm'].rolling(window= window_acc).mean()
-        gyro_all_df['low_angle'] = gyro_all_df['angle'].rolling(window=window_gyro).mean()
+        # 移動平均フィルタ
+        acce_all_df["low_norm"] = (
+            acce_all_df["norm"].rolling(window=window_acc_sec * acce_fs).mean()
+        )
+        gyro_all_df["low_angle"] = (
+            gyro_all_df["angle"].rolling(window=window_gyro_sec * gyro_fs).mean()
+        )
 
-        peaks, _ = signal.find_peaks(acce_all_df['low_norm'], distance=acce_fs*peak_distance_sec, height=peak_height)
+        # ピークの検出とプロット
+        peaks, _ = signal.find_peaks(
+            acce_all_df["low_norm"],
+            distance=acce_fs * peak_distance_sec,
+            height=peak_height,
+        )
 
-        track=[results[0]]
-        for p in peaks:
-            # 加速度と角速度のインデックス数に合わせる
-            time = acce_all_df['app_timestamp'][p]
-            gyro_i = gyro_all_df['app_timestamp'].sub(time).abs().idxmin()
+        track = [results[0]]
+        for peak in peaks:
+            time = acce_all_df["app_timestamp"][peak]
+            gyro_i = gyro_all_df["app_timestamp"].sub(time).abs().idxmin()
 
-            x = step * np.cos(gyro_all_df['angle'][gyro_i] + init_angle) + track[-1][0]
-            y = step * np.sin(gyro_all_df['angle'][gyro_i] + init_angle) + track[-1][1]
+            x = step * np.cos(gyro_all_df["angle"][gyro_i] + init_angle) + track[-1][0]
+            y = step * np.sin(gyro_all_df["angle"][gyro_i] + init_angle) + track[-1][1]
 
-            track.append(Position(x,y,0))
+            track.append(Position(x, y, 0))
+
+        # 推定結果を保存
         results.track = track
 
     results.plot_map()
